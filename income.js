cf7845d8e74eb9914f4f3d23a2004af1b6d7ea25
Kar1m113 <khaj0001@student.monash.edu>
2023-10-09 00:22:04 +1100

    Changes

---
+++ income.js
@@ -0,0 +1,5 @@
+var vg_1 = "Income.json";
+vegaEmbed("#Income", vg_1).then(function(result) {
+// Access the Vega view instance
+//(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
